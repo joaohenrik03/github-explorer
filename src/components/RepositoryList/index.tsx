f34464d2d4a3391ad66c1ac7c92e72db6845aec7
João Henrik <jh.signori@gmail.com>
2022-07-04 14:17:27 -0300

    Updated form and data fetching in API done

--- src/components/RepositoryList/index.tsx
+++ src/components/RepositoryList/index.tsx
@@ -1,20 +1,37 @@
+import { RepositoryItem } from '../RepositoryItem';
+import { useState } from 'react';
+import axios from 'axios';
+
 import { FaGithubSquare } from 'react-icons/fa'
 import { AiOutlineSearch } from 'react-icons/ai';
 import { Container } from './style';
-import { RepositoryItem } from '../RepositoryItem';
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+    const [user, setUser] = useState('');
+
+    function fetchUserRepositories() {
+        axios.get(`https://api.github.com/users/${user}/repos`)      
+        .then(response => setRepositories(response.data))
+        .catch(error => alert(error))
+    }
+
     return (
         <Container>
             <header>
                 <h1>
                     <FaGithubSquare />
-                    GitHub Explorer    
+                    GitHub Explorer   
                 </h1>    
 
-                <form>
+                <form onSubmit={fetchUserRepositories}>
                     <label htmlFor="userGitHub">Usuário(a):</label>  
-                    <input type="text" id="userGitHub" />      
+                    <input 
+                        type="text" 
+                        id="userGitHub" 
+                        onChange={event => setUser(event.target.value)}
+                        required
+                    />      
 
                     <button type="submit">
                         <AiOutlineSearch />
